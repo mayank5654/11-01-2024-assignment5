63642548ca45a172f755da777bcd55a241348d2f
mayank5654 <mayankkaushal99@gmail.com>
2024-01-11 15:01:06 +0530

    intial commit

---
+++ assignment5/MovieManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    internal class MovieManager
+    {
+        private List<Movie> movies;
+        public MovieManager()
+        {
+            // Initialize the movies list
+            movies = new List<Movie>
+        {
+            new Movie { Title = "hanuman", Director = "rudransh", Year = 2020 },
+            new Movie { Title = "shiva", Director = "mahadeva", Year = 2021 },
+            // Add more movies as needed
+        };
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+
+        public Movie GetMovieByTitle(string title)
+        {
+            return movies.FirstOrDefault(movie => movie.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
+
---
+++ assignment5/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using assignment5;
+
+
+MovieManager movieManager = new MovieManager();
+
+        Console.WriteLine("List of all movies:");
+        List<Movie> allMovies = movieManager.GetAllMovies();
+        PrintMovies(allMovies);
+
+        Console.WriteLine("\nEnter the title to get a specific movie:");
+        string title = Console.ReadLine();
+        Movie specificMovie = movieManager.GetMovieByTitle(title);
+
+        if (specificMovie != null)
+        {
+            Console.WriteLine($"\nDetails of movie '{title}':");
+            PrintMovie(specificMovie);
+        }
+        else
+        {
+            Console.WriteLine($"Movie with title '{title}' not found.");
+        }
+    
+
+    static void PrintMovies(List<Movie> movies)
+    {
+        foreach (var movie in movies)
+        {
+            PrintMovie(movie);
+        }
+    }
+
+    static void PrintMovie(Movie movie)
+    {
+        Console.WriteLine($"Title: {movie.Title}");
+        Console.WriteLine($"Director: {movie.Director}");
+        Console.WriteLine($"Year: {movie.Year}");
+        Console.WriteLine();
+    }
